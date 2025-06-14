6dc10c8a347c684464b2de5163db0b5d817a872c
Shiruvati <mrnattyboy05@gmail.com>
2025-06-14 12:25:58 +0530

    Update db.ts

--- src/server/db.ts
+++ src/server/db.ts
@@ -4,7 +4,6 @@ import { env } from "@/env";
 
 const createPrismaClient = () =>
   new PrismaClient({
-    log: env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
 const globalForPrisma = globalThis as unknown as {
