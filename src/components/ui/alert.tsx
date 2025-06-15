36eca44c8ab3ad514b5541a54c7d2ef2687dd99b
Shiruvati <mrnattyboy05@gmail.com>
2025-06-15 22:41:45 +0530

    Update alert.tsx

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -31,7 +31,6 @@ Alert.displayName = "Alert"
 
 const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
   <h5
     ref={ref}
